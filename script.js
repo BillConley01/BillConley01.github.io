fd31625d79114f4be1640a348c07f1a0e73d24f0
Bill Conley <88862631+BillConley01@users.noreply.github.com>
2021-10-27 11:57:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+    var tabList = [].slice.call(document.querySelectorAll(".navigation-list a:not(.dropdown-toggle)"));
+    tabList.forEach(element =>{
+       let elementID = element.getAttribute('href');
+       let pageLink = elementID.substring(1);
+       let page = document.getElementById(pageLink);
+       if(pageLink !=="home"){
+        page.classList.remove('show');
+        element.classList.remove('active');
+        console.log("other");
+       }
+       else console.log("home");
+        element.addEventListener("click", function(event){
+            console.log(pageLink);
+            event.preventDefault();
+            tabList.forEach(item =>{
+                let otherLink = item.getAttribute('href').substring(1);
+                if(otherLink !== pageLink){
+                    page = document.getElementById(otherLink);
+                    console.log("removing classes from " + otherLink);
+                    item.classList.remove('active');
+                    page.classList.remove('active');
+                    page.classList.remove('show');
+                }
+                else  {
+                    console.log("adding classes to " + otherLink);
+                    element.classList.add('active');
+                    page = document.getElementById(otherLink);
+                    page.classList.add('show');
+                    page.classList.add('active');
+                }
+            });
+        });
+    });
+});
