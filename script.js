c306bfc99b7f32655e14ee1714a49e4b624ff5c3
Bill Conley <88862631+BillConley01@users.noreply.github.com>
2021-11-01 15:17:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,23 +8,18 @@ document.addEventListener("DOMContentLoaded", function(){
        if(pageLink !=="home"){
         page.classList.remove('show');
         element.classList.remove('active');
-        console.log("other");
        }
-       else console.log("home");
         element.addEventListener("click", function(event){
-            console.log(pageLink);
             event.preventDefault();
             tabList.forEach(item =>{
                 let otherLink = item.getAttribute('href').substring(1);
                 if(otherLink !== pageLink){
                     page = document.getElementById(otherLink);
-                    console.log("removing classes from " + otherLink);
                     item.classList.remove('active');
                     page.classList.remove('active');
                     page.classList.remove('show');
                 }
                 else  {
-                    console.log("adding classes to " + otherLink);
                     element.classList.add('active');
                     page = document.getElementById(otherLink);
                     page.classList.add('show');
@@ -36,9 +31,12 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 document.addEventListener("DOMContentLoaded", function(){
-const link = document.querySelector('README.md');
-// Clone the <template> in the import.
-const template = link.import.querySelector('template');
-const clone = document.importNode(template.content, true);
-document.querySelector('#container').appendChild(clone);
+let link = document.querySelector('README');
+    if(link){
+        console.log("link not null " + link);
+        let template = link.import.querySelector('template');
+        let clone = document.importNode(template.content, true);
+        document.querySelector('#template-container').appendChild(clone);
+    }
+console.log("link null ");
 });
