8f24c8d88c3554708e1d0fd98d29c11213a513e2
Bill Conley <88862631+BillConley01@users.noreply.github.com>
2021-11-01 20:02:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -34,9 +34,9 @@ window.addEventListener('load', function(){
 let link = document.querySelector('link[rel="import"]');
     if(link){
         console.log("link not null " + link);
-        let template = link.import.querySelector('README');
+        let template = link.import.querySelector('template');
         let clone = document.importNode(template.content, true);
-        document.querySelector('#template-container').appendChild(clone);
+        document.getElementById('template-container').appendChild(clone);
     }
 console.log("link null ");
 });
